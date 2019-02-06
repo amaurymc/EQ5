3547c091464cd021d099463ec5bf51322e7bfd1f
amc <46652567+amaurymc@users.noreply.github.com>
2019-02-05 19:16:31 -0600

    Update and rename Eq5.Amaury.2.c to Eq5.Ejercicio41.Amaury.c

---
+++ Eq5.Ejercicio41.Amaury.c
@@ -0,0 +1,75 @@
+/*
+Autor: AMAURY MORALES CERECEDO
+
+Entrada: Cualquier numero entero, ya sea positivo o negativo.
+
+Salida: El numero mas grande y el mas chico de todos los numeros dados por el usuario
+
+Procedimiento general: Se van cambiando los tronos correspondientes dependiendo de los numeros que el usuario introduzca.
+Al principio se configuran los 2 tronos como el primer numero dado para evitar problemas.
+
+Notas: No se admiten numeros que no sean enteros. Al principio se inicializan los dos tronos con el primer numero porque al introducir
+todos los numeros como negativos, el trono seguira siendo 0 aunque nunca se haya puesto en la lista de numeros a comparar.
+No estoy seguro de cuantos numeros pueda comparar, pero estableci como maximo 99999999 en caso de que por memoria no se puedan realizar mas.
+
+*/
+
+//PREMISA//
+/*Escriba  el programa que  lea  N  números  y  calcule  cual  es  el  mayor  y  el  menor de esos números.
+Utilice una estructura de repetición Para...hasta.*/
+
+#include <stdio.h>
+
+//DECLARACIONES//
+int num; //Numero a introducir por el usuario.
+int cont; //Contador para nuestro ciclo.
+int tope; //Total de numeros a introducir.
+
+int tronoMin = 0; //Trono del numero mas pequeño.
+int tronoMax = 0; //Trono del numero mas grande.
+
+int lista[99999999]; //Lista de todos los numeros introducidos por el usuario
+
+int main()
+{
+ //ENTRADAS//
+ printf("Por favor, introduzca el total de numeros a comparar: ");
+ scanf("%d", &tope);
+ ////////////
+
+ for (cont = 0; cont < tope; cont++)
+ {
+  //ENTRADAS//
+  printf("Por favor, introduzca un numero: ");
+  scanf("%d", &num);
+  ////////////
+
+
+  ///PROCEDIMIENTO///
+  lista[cont] = num;
+
+  if (cont == 0)
+  {
+   tronoMin = num;
+   tronoMax = num;
+  }
+  if (num < tronoMin)
+  {
+   tronoMin = num;
+  }
+  if (num > tronoMax)
+  {
+   tronoMax = num;
+  }
+  ////////////////////
+ }
+
+ //SALIDA//
+ printf("Numero mayor: %d\n", tronoMax);
+ printf("Numero menor: %d\n", tronoMin);
+ //////////
+
+ scanf("%d", &tope); //GetChar no me funciono aqui, asi que solo reescribo una variable.
+
+ return 0;
+}

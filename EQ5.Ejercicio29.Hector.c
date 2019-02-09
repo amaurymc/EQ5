d09090d570f3ba133fb90a176cbcd341076793ff
HectorCosgalla <valcos98@hotmail.com>
2019-02-09 15:33:46 -0600

    Update EQ5.Ejercicio29.Hector.c

--- EQ5.Ejercicio29.Hector.c
+++ EQ5.Ejercicio29.Hector.c
@@ -1,4 +1,17 @@
-int int main() {
-  /* code */
+#include <stdio.h>
+int main() {
+  int Numero, Divisor = 2;
+  printf("Dame un numero:\n" );
+  scanf("%d", &Numero);
+  while (Numero>=Divisor)
+  {
+    while (Numero%Divisor == 0)
+    {
+      printf("%d x ", Divisor);
+      Numero = Numero/Divisor;
+    }
+    Divisor = Divisor+1;
+  }
+  printf("%d", Numero);
   return 0;
 }

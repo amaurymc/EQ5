7d3f7c78577ea6ec6c4ff808efb22882a2cdea9b
HectorCosgalla <valcos98@hotmail.com>
2019-02-10 16:50:11 -0600

    Update EQ5.Ejercicio35.Hector.c

---
+++ EQ5.Ejercicio35.Hector.c
@@ -0,0 +1,14 @@
+int int main() {
+  int n, nmenor;
+  printf("Ingresa un numero:\n");
+  scanf("%d\n", &n);
+  nmenor = n - 1
+  for (i=0; i = nmenor; i++) {
+    for (m = 0; m = nmenor ;m++) {
+      if (i <= m) {
+        printf("%d  %d\n",i m );
+      }
+    }
+  }
+  return 0;
+}
